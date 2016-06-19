475db2100df99977c9465f65e46e8cbfa06de953
Paul Savignano <Paul.Savignano@gmail.com>
2016-06-19 10:48:06 -0700

    Added image list and detail components.

---
+++ images3/client/components/image_list.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import ImageDetail from './image_detail'
+
+const ImageList = () => {
+  return (
+    <ul>
+      <ImageDetail />
+    </ul>
+  )
+}
+
+export default ImageList
