5330029570d8d36b821a5cf54ccbe99ba075691d
Paul Savignano <Paul.Savignano@gmail.com>
2016-06-20 16:58:57 -0700

    Added components and axios ajax.

--- images3/client/components/image_list.js
+++ images3/client/components/image_list.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
 import ImageDetail from './image_detail'
 
-const ImageList = () => {
+const ImageList = (props) => {
+  const validImages = props.images.filter((image) => !image.is_album)
+  const RenderedImages = validImages.map((image) => {
+    return <ImageDetail image={image} key={image.title} />
+  })
   return (
-    <ul>
-      <ImageDetail />
+    <ul className="media-list list-group">
+      {RenderedImages}
     </ul>
   )
 }
--- images3/client/main.js
+++ images3/client/main.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
+import axios from 'axios'
 import ImageList from './components/image_list'
 
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  )
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { images: [] }
+  }
+  componentWillMount() {
+    this.view = axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => this.setState({ images: response.data.data }))
+  }
+  componentWillUnmount() {
+    this.view = ''
+  }
+  render() {
+    return (
+      <div>
+        <ImageList images={this.state.images} />
+      </div>
+    )
+  }
 }
 
 Meteor.startup(() => {
